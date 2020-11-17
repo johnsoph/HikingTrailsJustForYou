6ec167a52d78fe5795115e5a8a0cd0d509161779
Kalise Ball <kalise@Kalises-MacBook-Pro.local>
2020-11-16 18:17:07 -0800

    updating the api call for the hikes to actually trigger the aciton and store the hikes in the store

--- client/src/redux/actions.ts
+++ client/src/redux/actions.ts
@@ -1,9 +1,9 @@
 import { INIT_HIKES, TOGGLE_USER } from "./action-types";
 
-export const initHikes = (payload)  => {
-    debugger;
-    return ({ type: INIT_HIKES, payload })
-};
+export const initHikes = (payload)  => ({ 
+    type: INIT_HIKES, 
+    payload,
+});
   
 
 export const toggleUser = (payload) => ({
--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,5 +1,5 @@
-
-import {initHikes} from '../redux/actions'
+import store from '../redux/store'
+import { initHikes } from '../redux/actions'
 
 const hikeURL = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200968807-358e68bfeeebd4e58f0e1c1155369e11";
 
@@ -8,8 +8,7 @@ export function callAPI(){
       .then(res => res.json())
       .then(
         (result) => {
-            debugger;
-            initHikes(result.trails)
+            store.dispatch(initHikes(result.trails))
         //   this.setState({
         //     isLoaded: true,
         //     items: result.items
