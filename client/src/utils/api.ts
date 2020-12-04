9f15bf3956ad6222ba5d8b39f61df08238721ef2
Kalise Ball <kalise@Kalises-MacBook-Pro.local>
2020-12-03 19:12:15 -0800

    adding max results = 50

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -4,7 +4,7 @@ import { initHikes } from '../redux/actions'
 //const hikeURL = "https://www.hikingproject.com/data/get-trails?lat=44.2590582&lon=-121.2171486&maxDistance=10&key=200968807-358e68bfeeebd4e58f0e1c1155369e11";
 
 export function callAPI(lat, lon){
-  var coord = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=200968807-358e68bfeeebd4e58f0e1c1155369e11`;
+  var coord = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&maxResults=50&key=200968807-358e68bfeeebd4e58f0e1c1155369e11`;
     fetch(coord)
       .then(res => res.json())
       .then(
